b1b917c2cca9ad33e007fe0bff543a1fd17779c8
Frandevep <frandevep@gmail.com>
2025-03-30 02:49:59 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1=0;
+    int num2=0;
+    int resu=0;
+    cout<<"Calculadora de sumas"<< endl;
+    cin>>num1;
+    cout<<"Tu primer numero es:"<<num1;
+    cout<<"\nEscriba el segundo numero"<< endl;
+    cin>>num2;
+    cout<<"Tu segundo numero es:"<<num2;
+    resu=num1+num2;
+    cout<<"\nLa suma de los numeros es:"<<resu;
+
+    return 0;
+}
